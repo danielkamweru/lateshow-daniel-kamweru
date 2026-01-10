a7a68e6dbbbfa45bc0c65650242d9c6d2b30fc09
danielkamweru <daniel.kamweru@student.moringaschool.com>
2026-01-10 13:38:04 +0300

    Add Episode model with basic fields and relationships

--- models.py
+++ models.py
@@ -1,48 +1,54 @@
 from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
-from . import  db
-class Episode(db.Model, SelializerMixin):
+from __init__ import db
+
+class Episode(db.Model, SerializerMixin):
     __tablename__ = "episodes"
+    
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String)
-    number = db.Column(db.Integer)
-
-    appearances = db.relationship(
-        "Apperance",
-        back_populates = "episode",
-        cascade ="all, delete-orphan"
-    )
-    serialize_rules = ("-appearances.episode",)
-class Guest(db.Model, SerializerMixin):
-    __tablename__ = "guests"
-
-    id = db.Column(db.Interger, primary_key= True)
-    name = db.Column(db.String)
-    occupation = db.Column(db.String)
-
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+    
     appearances = db.relationship(
         "Appearance",
-        back_populates = "guest",
-        cascade = "all, delete-orphan"
+        back_populates="episode",
+        cascade="all, delete-orphan"
     )
-    serialize_rules = ("-apperances.guest",)
+    
+    serialize_rules = ("-appearances.episode",)
+
+class Guest(db.Model, SerializerMixin):
+    __tablename__ = "guests"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+    
+    appearances = db.relationship(
+        "Appearance",
+        back_populates="guest",
+        cascade="all, delete-orphan"
+    )
+    
+    serialize_rules = ("-appearances.guest",)
+
 class Appearance(db.Model, SerializerMixin):
     __tablename__ = "appearances"
-
+    
     id = db.Column(db.Integer, primary_key=True)
-    rating = db.Column(db.Integer)
-
-    episode_id = db.Column(db.Integer, db.ForeignKey("episodes.id"))
-    guest_id = db.Column(db.Integer, db.ForeignKey("guests.id"))
-
+    rating = db.Column(db.Integer, nullable=False)
+    
+    episode_id = db.Column(db.Integer, db.ForeignKey("episodes.id"), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey("guests.id"), nullable=False)
+    
     episode = db.relationship("Episode", back_populates="appearances")
     guest = db.relationship("Guest", back_populates="appearances")
-
+    
     serialize_rules = (
         "-episode.appearances",
         "-guest.appearances",
     )
-
+    
     @validates("rating")
     def validate_rating(self, key, rating):
         if not isinstance(rating, int) or rating < 1 or rating > 5:
