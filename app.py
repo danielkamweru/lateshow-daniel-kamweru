787315e9bb73eb78e9755aa5d447ac075f4a4ac6
danielkamweru <daniel.kamweru@student.moringaschool.com>
2026-01-10 12:37:02 +0300

    added a get request

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ from flask import request, jsonify
 from . import create_app, db
 from . models import Episode,Guest, Appearances
 
+app = create_app()
+@app.route("/episodes", methods=["GET"])
+def get_episodes():
+    espisodes = Episode.query.all()
+    return jsonify([e.to_dict(only=("id","date", "number")) for e in episodes]), 200
+
