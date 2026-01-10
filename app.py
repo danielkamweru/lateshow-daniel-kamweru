4a5530c487483303b85e97f021654e9cfd91c218
danielkamweru <daniel.kamweru@student.moringaschool.com>
2026-01-10 12:46:37 +0300

    added the GET method

--- app.py
+++ app.py
@@ -7,4 +7,15 @@ app = create_app()
 def get_episodes():
     espisodes = Episode.query.all()
     return jsonify([e.to_dict(only=("id","date", "number")) for e in episodes]), 200
+@app.route("/episodes/<int:id>", methods=["GET"])
+def get_episode(id):
+    episode = Episode.query.get(id)
+    if not episode:
+        return jsonify({"error":"Episode not found"}), 404
 
+    return jsonify(
+        episode.to_dict(
+            only=("id", "date", "number", "appearances"),
+            rules=("-appearances.episode",)
+        )
+    ), 200
